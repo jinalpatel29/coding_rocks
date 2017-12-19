f2609bb053b0cc371d66c3b985eeae571c0e2084
Jayanth Kanive <mailtokanive@gmail.com>
2017-12-19 10:53:50 -0800

    add config.json settings

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -2,7 +2,14 @@ var mongoose = require('mongoose');
 var fs = require('fs');
 var path = require('path');
 
-mongoose.connect('mongodb://localhost/survey_app');
+var config = require('./config.json');
+
+console.log(config.mongodb.username);
+
+var uri = `mongodb://${config.mongodb.username}:${config.mongodb.password}@codingrocks-shard-00-00-qbt5l.mongodb.net:27017,codingrocks-shard-00-01-qbt5l.mongodb.net:27017,codingrocks-shard-00-02-qbt5l.mongodb.net:27017/testDB?ssl=true&replicaSet=CodingRocks-shard-0&authSource=admin`;
+mongoose.connect(uri);
+
+// mongoose.connect('mongodb://localhost/survey_app');
 mongoose.connection.on('connected', () => console.log('connected to MongoDB'));
 mongoose.Promise = global.Promise;
 
