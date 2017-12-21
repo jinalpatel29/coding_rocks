f195fa8e7f8c0db04b6e5d9a49abac1e0f7e899c
jinalpatel29 <jinalpatel29@yahoo.co.in>
2017-12-20 19:33:20 -0800

    added Mailer changes and removed unused code

--- angular-app/src/app/invite/invite.component.ts
+++ angular-app/src/app/invite/invite.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-invite',
@@ -6,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./invite.component.css']
 })
 export class InviteComponent implements OnInit {
-
-  constructor() { }
-
+  email;
+  constructor(private _service:DataService) { }
+  onSubmit(){
+    this._service.invite(this.email);
+  }
   ngOnInit() {
   }
 
--- server.js
+++ server.js
@@ -3,20 +3,54 @@ var app = express();
 // var moment = require('moment');
 
 var path = require('path');
-// var cors = require('cors');
-// app.use(cors());
-app.use(express.static(__dirname + '/angular-app/dist'));
+var mailer = require('express-mailer');
 
+app.use(express.static(__dirname + '/angular-app/dist'));
+app.set('views', __dirname + '/server/views');
+app.set('view engine', 'ejs');
 var parser = require('body-parser');
 app.use(parser.json());
-
 var mongoose = require('mongoose');
 require('./server/config/mongoose.js');
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+mailer.extend(app, {
+    from: 'no-reply@lovefool.com',
+    secureConnection: true, // use SSL 
+    port: 537, // port for secure SMTP 
+    transportMethod: 'SMTP', // default is SMTP. Accepts anything that nodemailer accepts 
+    auth: {
+        user: 'lovefoolteam@gmail.com',
+        pass: 'codingdojo'
+    }
+});
+
+app.post('/invite', function (req, res) {   
+    console.log("in server invite "); 
+    app.mailer.send('email', {
+        to: req.body.email,//'example@example.com', // REQUIRED. This can be a comma delimited string just like a normal email to field.  
+        subject: 'Lovefool Invite Email', // REQUIRED. 
+        otherProperty: 'Other Property' // All additional properties are also passed to the template as local variables. 
+    }, function (err) {
+        if (err) {
+            // handle error 
+            console.log(err);
+            res.send('There was an error sending the email');
+            return;
+        }
+        res.send('Email Sent');
+        // res.redirect('/');
+    });
+})
 
 var routes_setter = require('./server/config/routes.js');
 
 routes_setter(app)
-
 const port = 8000;
 // console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
-app.listen(port, ()=> console.log('Express server listening on port ${port}'));
+app.listen(port, () => console.log('Express server listening on port ${port}'));
