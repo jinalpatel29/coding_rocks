a84741bab9045c8f15ad3ef37840f5ba61d6aa81
kegplant <ace1905@gmail.com>
2017-12-21 20:26:15 -0800

    grabbing partners now

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -57,6 +57,16 @@ module.exports = {
             }
         });
     },
+    findOneByID: function (req, res){
+        User.findOne({ _id: req.params.user_id }, function(err, user){
+            if (err){
+                console.log(err);
+            } else {
+                console.log("got user");
+                res.json(user);
+            }
+        });
+    },
     addRequest: function (req, res){
         User.update({_id: req.params.id}, { $push: { requests: req.body.user_id }}, function(err, result){
             if (err){
